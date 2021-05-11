8c77b482a939f3618fa0af9fe8d191842d176cb6
Lepester <slavik55350@gmail.com>
2021-05-11 20:54:07 +0300

    chonk chart

---
+++ Task1/src/com/company/WildCat.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class WildCat extends Cat {
+    public String getName() {
+        return "Avilon";
+    }
+
+    public String getBreed() {
+        return "Smite";
+    }
+
+    public void hatePeople(){
+        System.out.println("Wild " + getName() + " " + getBreed() + " hates people");
+    }
+}
+            //sisi
