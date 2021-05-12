7bff5840cc8549d73c1ff45aa5e419247e00e211
Lepester <slavik55350@gmail.com>
2021-05-12 14:45:35 +0300

    Story about two cats

--- Task1/src/com/company/HomeCat.java
+++ Task1/src/com/company/HomeCat.java
@@ -1,16 +1,44 @@
 package com.company;
 
 public class HomeCat extends Cat {
+
     public String getName() {
-        return "Bebra";
+        return "Herbert";
     }
 
     public String getBreed() {
-        return "Sis`";
+        return "Munchkin";
     }
 
     public void layOnSofa(){
-        System.out.println("Fat motherfucker " + getName() + " " + getBreed() + " is laying on sofa");
+        System.out.println("It was a sunny day. Our cute cat is laying on sofa peacefully. He's name is " + getName());
+        System.out.println("Thick and fluffy fur makes him very adorable.");
+        System.out.println(" ");
+        System.out.println("--I'm going to work, so be a pretty kitty, " + getName() + ".");
+        System.out.println("--Meooww, said Herbert.");
+        System.out.println(" ");
+        System.out.println("|| Master closed the door and left the home ||");
+        System.out.println(" ");
+        System.out.println("Time has passed. " + getName() + " thought to myself: \"--I'm bored\".");
+        System.out.println("Suddenly he looked at the door's side. To be more precise he looks at the shoes which lies there.");
     }
 
+    public void spoilOnShoes() {
+        while (money > 2) {
+            System.out.println("Herbert is a tricky " + getBreed() + " cat, who likes to be sometimes a bad boi.");
+            System.out.println("Herbert stood up and start to go right to the shoes.");
+            System.out.println(" ");
+            System.out.println("P.S. Herbert has an ability to spoil on shoes, so " + master + " looses his money. Herbert calls it 「The CASH」.");
+            System.out.println(" ");
+            System.out.println("Herbert thought to myself: \"I need to do this! Leess GO, 「The CASH」!\"");
+            System.out.println(" ");
+            System.out.println("P.P.S. Main ability of 「The CASH」 is MONEY BACK. It's stealing the money from it's owner and return to the " + getName() + ".");
+            System.out.println(" ");
+            System.out.println("|| " + master + " has lost the money ||");
+            System.out.println(master + " said: \"--Oh noooo! My money collection!!!\"");
+            System.out.println("----------------------------------------------------------------");
+            money--;
+        }
+    }
 }
+
--- Task1/src/com/company/WildCat.java
+++ Task1/src/com/company/WildCat.java
@@ -2,15 +2,43 @@ package com.company;
 
 public class WildCat extends Cat {
     public String getName() {
-        return "Avilon";
+        return "Guts";
     }
 
     public String getBreed() {
-        return "Smite";
+        return "Maine Coon";
     }
 
     public void hatePeople(){
-        System.out.println("Wild " + getName() + " " + getBreed() + " hates people");
+        System.out.println(" ");
+        System.out.println(getName() + " is a wild " + getBreed() + " cat, who lives in the same town as Herbert.");
+        System.out.println(getName() + " has no home cause he's owner left him, when " + getName() + " was only 2 months.");
+        System.out.println("Actually " + getName() + " hates people. " + "Despite of that " + getName() + " is a kind cat. He helps a lot to Morioh's residents.");
+        System.out.println(" ");
+        System.out.println("P.S. Morioh is a town located in Japan. There are only 27 residents live, including the Master.");
+        System.out.println(" ");
+        System.out.println("Exactly on this day " + getName() + " walks on the streets of Morioh.");
+        System.out.println(" ");
+        System.out.println("\"--Oh noooo! My money collection!!!\"");
+        System.out.println(getName() + " thought to myself: \"--What? There can be no doubt. This is a work of stand\".");
+        System.out.println(" ");
+    }
+
+    public void saveWorld() {
+        System.out.println(getName()+ "' life was not easy from very birth...");
+        System.out.println("--Another cat was born. Another life will be wasted, said his Owner.");
+        System.out.println("--You can't just leave him here! Why can't we take him away with us?");
+        System.out.println("--I told you why, Nancy! Stop it!");
+        System.out.println("...");
+        System.out.println(" ");
+        System.out.println("Memories flew through " + getName() + "' head.");
+        System.out.println("I don't want him to feel bad. I must help him, thought " + getName() + ".");
+        System.out.println("--With my stand I can do it. Come on,「The WORLD」");
+        System.out.println(" ");
+        System.out.println("P.P.S ability of his stand is to erase the time and leave only result of it.");
+        System.out.println(" ");
+        System.out.println(master + ": oh, here is my money.");
+        System.out.println("--Hey, Mista, are you going to drink coffee with us?");
+        System.out.println("--Sure!");
     }
 }
-            //sisi
